c70b1340a556ec511a6525c22dfd7d83fc4c799e
Pratiyush Ray <pkray0021@gmail.com>
2025-12-18 11:36:13 +0530

    CORS Issue Resolving

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const auth = require('./routes/auth');
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL ? process.env.CLIENT_URL.replace(/\/$/, "") : 'http://localhost:5173',
     credentials: true,
 }));
 
